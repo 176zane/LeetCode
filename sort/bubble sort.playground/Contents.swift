cb789359a84936f8aa1abf63bae96f0d3dfc0b06
姚七六 <176zane@gmail.com>
2022-09-20 17:15:03 +0800

    排序方法修改

--- sort/bubble sort.playground/Contents.swift
+++ sort/bubble sort.playground/Contents.swift
@@ -20,3 +20,25 @@ var s = Solution()
 var nums = [2, 3, 5, 7, 4, 8, 6 ,10 ,1, 9]
 s.bubbleSort(&nums)
 print(nums)
+
+
+
+func bubbleSort(_ array: [Int]) ->[Int] {
+    var sortedArray = array
+    for i in 0..<sortedArray.count {
+        var flag = true
+        for j in 0..<sortedArray.count-i-1{
+            if sortedArray[j] > sortedArray[j+1] {
+                let temp = sortedArray[j+1]
+                sortedArray[j+1] = sortedArray[j]
+                sortedArray[j] = temp
+                flag = false
+            }
+        }
+        if flag {break}
+    }
+    return sortedArray
+}
+
+let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+bubbleSort(list)
--- sort/insertion sort.playground/Contents.swift
+++ sort/insertion sort.playground/Contents.swift
@@ -15,3 +15,23 @@ var s = Solution()
 var nums = [2, 3, 5, 7, 4, 8, 6 ,10 ,1, 9]
 s.insertionSort(&nums)
 print(nums)
+
+
+//
+
+func insertionSort(_ array: [Int]) -> [Int] {
+    var sortedArray = array
+    for i in 1..<sortedArray.count {
+        var j = i
+        let temp = sortedArray[j]
+        while j > 0 && sortedArray[j - 1] > temp {
+            sortedArray[j] = sortedArray[j - 1]                // 1
+            j -= 1
+        }
+        sortedArray[j] = temp                      // 2
+    }
+    return sortedArray
+}
+
+let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+insertionSort(list)
