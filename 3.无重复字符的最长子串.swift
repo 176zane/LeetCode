8964eb83937bb93e0efc3354ca4face3039c6f2c
176zane <176zane@gmail.com>
2023-06-14 16:08:57 +0800

    3

---
+++ 3.无重复字符的最长子串.swift
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=swift
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var max = 0
+        var cArray : [Character] = []
+        for c in s {
+            if let index = cArray.firstIndex(of: c) {
+                cArray.removeSubrange(0...index)
+            }
+            cArray.append(c)
+            max = cArray.count > max ? cArray.count : max
+        }
+        
+        return max
+    }
+}
+class Solution2{
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        
+        var max = 0
+        let cArray = Array(s)
+        
+        for i in 0..<cArray.count {
+            var cSet: Set<Character>  = []
+            for j in i..<cArray.count {
+                if cSet.contains(cArray[j]) {
+                    let v = j - i
+                    max = v > max ? v : max
+                    cSet = []
+                    break
+                }else {
+                    cSet.insert(cArray[j])
+                    
+                }
+            }
+            if cSet.count > max {//字符串中没有重复的字符
+                max = cSet.count
+            }
+        }
+        return max
+    }
+}
+// @lc code=end
+
