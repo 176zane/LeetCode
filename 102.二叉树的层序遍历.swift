a7132fa1d825d0b4cb72ab8c317738c8c6f68873
176zane <176zane@gmail.com>
2023-07-14 13:55:15 +0800

    102

---
+++ 102.二叉树的层序遍历.swift
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=swift
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        var result = [[Int]]()
+        var queue = [TreeNode]()
+        if let node = root {
+            queue.append(node)
+        }
+        while queue.count > 0 {
+            var nodeArray = [TreeNode]()
+            while queue.count > 0 {
+                let node = queue.removeFirst()
+                nodeArray.append(node)
+            }
+            var valArray = [Int]()
+            for node in nodeArray {
+                if let left = node.left {
+                    queue.append(left)
+                }
+                if let right = node.right {
+                    queue.append(right)
+                }
+                valArray.append(node.val)
+            }
+            result.append(valArray)
+        }
+        return result
+    }
+}
+// @lc code=end
+
