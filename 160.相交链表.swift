f16b675bc843a0fc94829a49181b09801c8a42cd
176zane <176zane@gmail.com>
2023-09-16 22:59:13 +0800

    1660

---
+++ 160.相交链表.swift
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=swift
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
+        if headA == nil || headB == nil {
+            return nil
+        }
+        
+        var pA = headA, pB = headB
+        while pA !== pB {
+            pA = pA == nil ? headB : pA?.next
+            pB = pB == nil ? headA : pB?.next
+        }
+        return pA
+    }
+}
+
+// @lc code=end
+
