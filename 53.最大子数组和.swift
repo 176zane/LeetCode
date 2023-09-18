79fd1c11557c14ea9cb02c2029fed1d63fd27e7d
176zane <176zane@gmail.com>
2023-09-18 19:11:11 +0800

    53

---
+++ 53.最大子数组和.swift
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=swift
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+class Solution {
+    func maxSubArray(_ nums: [Int]) -> Int {
+        var result = nums[0]
+        var dp = Array(repeating: 0, count: nums.count)
+        dp[0] = nums[0]
+        //dp[i]表示以i为结尾（包含i）的字串的最大值，而不是通常认为的i之前的最大值（不一定包含i）
+        //因此最后返回的最大值也不是dp[n-1],而是去dp数组中找最大值
+        for i in 1..<nums.count {
+            if dp[i-1] >= 0 {
+                dp[i] = dp[i-1] + nums[i]
+            }else {
+                dp[i] = nums[i]
+            }
+            result = max(result, dp[i])
+        }
+        return result
+    }
+}
+
+class Solution2 {
+    func maxSubArray(_ nums: [Int]) -> Int {
+        var result = nums[0], currentMax = nums[0]
+        for i in 1..<nums.count {
+            currentMax = max(currentMax + nums[i], nums[i])
+            result = max(result, currentMax)
+        }
+        return result
+    }
+}
+
+// @lc code=end
+
